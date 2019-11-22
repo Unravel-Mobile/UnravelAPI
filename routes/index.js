7b957b32f305f51592a195eaf2772c4cf567986b
Jenn June <jennjuneux@gmail.com>
2019-11-21 20:30:08 -0800

    added route for anonymous authentication

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,31 @@ var Router = express.Router();
 var db = require('../models');
 
 
+//Sign in Authentication Route
+
+Router.post("/signin", function (req, res) {
+    // Create a new User in the db
+    db.User.create(req.body)
+        .then(function (dbUser) {
+            // If a User was created successfully, find one User (there's only one) and push the new User's _id to the User's `Users` array
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            console.log("==========")
+            console.log(dbUser);
+            console.log("==========")
+
+        })
+        .then(function (dbUser) {
+            // If the User was updated successfully, send it back to the client
+            res.json(dbUser);
+        })
+        .catch(function (err) {
+            // If an error occurs, send it back to the client
+            res.json(err);
+        });
+});
+
+
 // Route for saving a new thougt to the db and associating it with a User
 Router.post("/thoughts", function (req, res) {
     // Create a new Thought in the db
@@ -52,7 +77,7 @@ Router.delete('/thoughts/:_id', function (req, res) {
         .catch(err => {
             console.log(err)
         })
-        
+
 });
 
 

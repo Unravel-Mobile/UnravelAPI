b786b2aa360b281358538b730d816d7c7165aec8
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-01-20 18:23:50 -0800

    adds some deletes some console logs

--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,9 @@ Router.post("/signin", function (req, res) {
             // If a User was created successfully, find one User (there's only one) and push the new User's _id to the User's `Users` array
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            console.log("+++++++++++++")
-            console.log(dbUser);
-            console.log("+++++++++++")
+            // console.log("+++++++++++++")
+            // console.log(dbUser);
+            // console.log("+++++++++++")
 });
 
 Router.get("/", function (req, res) {
@@ -31,18 +31,21 @@ Router.get("/", function (req, res) {
     res.send('WELCOME TO THE UNRAVEL APP® API');
 });
 
-Router.get('/user/thoughts/:id', function (req, res) {
-    console.log('REQ / RES / REQ / RES / REQ / RES / REQ / RES');
+Router.get('/user/thoughts/:_id', function (req, res) {
+    console.log('* *  R E Q   B E L O W   T H I S   L I N E * *');
     console.log(req);
+    console.log('*  * R E Q   A B O V E   T H I S   L  I N E * *');
+
+    console.log('* *  R E S   B E L O W   T H I S   L I N E * *');
     console.log(res);
-    console.log('REQ / RES / REQ / RES / REQ / RES / REQ / RES');
+    console.log('*  * R E S   A B O V E   T H I S   L  I N E * *');
 
     db.User.findById(req.params._id)
         .populate('thoughts')
         .then(function (dbSaved) {
-            console.log("********************")
-            console.log(dbSaved);
-            console.log("**************");
+            // console.log("********************")
+            // console.log(dbSaved);
+            // console.log("**************");
             res.json(dbSaved);
         })
         .catch(function (err) {
@@ -54,18 +57,18 @@ Router.get('/user/thoughts/:id', function (req, res) {
 
 // Route for saving a new thougt to the db and associating it with a User
 Router.post("/thoughts", function (req, res) {
-    console.log(req);
-    console.log(res);
+    // console.log(req);
+    // console.log(res);
     // Create a new Thought in the db
     db.Thought.create(req.body)
         .then(function (dbThought) {
             // If a Thought was created successfully, find one User (there's only one) and push the new Thought's _id to the User's `thoughts` array
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            console.log("==========");
-            console.log(dbThought);
-            console.log("==========");
-            console.log('req body userId -> ', req.body.userId);
+            // console.log("==========");
+            // console.log(dbThought);
+            // console.log("==========");
+            // console.log('req body userId -> ', req.body.userId);
             // .update({_id: contact.id}, upsertData
             return db.User.updateOne({ _id: req.body.userId }, { $push: { thoughts: dbThought._id } }, { new: true });
             // return 'YAHOO';
@@ -84,9 +87,9 @@ Router.post("/thoughts", function (req, res) {
 Router.get('/thoughts/:_id', function (req, res) {
     db.Thought.findById({_id: req.params._id})
         .then(function (dbSaved) {
-            console.log("********************")
-            console.log(dbSaved.thoughtId);
-            console.log("**************");
+            // console.log("********************")
+            // console.log(dbSaved.thoughtId);
+            // console.log("**************");
             res.json(dbSaved);
         })
         .catch(function (err) {

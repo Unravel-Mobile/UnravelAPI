7afd9a0e1fc0c2ef0551616524bf31eb4443cd8c
Blue Bean <sl8305@hotmail.com>
2020-02-13 18:33:38 -0800

    comments for index.js routes

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,6 @@ var path = require("path");
 //Sign in Authentication Route
 
 Router.post("/signin", function (req, res) {
-    // console.log(' SIGN IN  R E Q ');
-    // console.log(req);
-    // console.log('SIGN IN  REQ');
-    // console.log(' SIGN IN  R E S ');
-    // console.log(res),
-    // console.log(' SIGN IN  R E S ');
 
     // Create a new User in the db
     db.User.create(req.body)
@@ -22,9 +16,7 @@ Router.post("/signin", function (req, res) {
     // If a User was created successfully, find one User (there's only one) and push the new User's _id to the User's `Users` array
     // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
     // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-    // console.log("+++++++++++++")
-    // console.log(dbUser);
-    // console.log("+++++++++++")
+
 });
 
 Router.get("/", function (req, res) {
@@ -33,6 +25,7 @@ Router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// finds the thoughts of the user with their userId
 Router.get('/user/thoughts/:userId', function (req, res) {
 
     // db.User.find() returns an array of all matches as result.
@@ -45,67 +38,45 @@ Router.get('/user/thoughts/:userId', function (req, res) {
             console.log("USER ID: ", dbuser.userId);
             console.log("USER THOUGHTS: ", dbuser.thoughts);
 
+            // if the user has thoughts 
+            // first: take each thought to $in
+            // second: for each $in, find all _id that matches db.Thought
+            // third: for all _id found, populate "Thought"
+            // fourth: after all "Thought" populated, execute fcn
+            // fifth: function logs and sends responses.
             if (dbUserThoughts && dbUserThoughts.length) {
                 db.Thought.find({ _id: { $in : dbUserThoughts }}).populate("Thought").exec(
                     function(err, dbthoughts) {
+                        // logs errors
                         if (err) console.log(err); 
                         console.log("THOUGHTS IN DB", dbthoughts);
                         res.json(dbthoughts);
                 });
             } else {
+                // logs if user has no thoughts
                 console.log("Something wrong with user thoughts id: ", dbUserThoughts);
             }
         });
-    
-
-    // db.User.findOne({ userId: req.params._id })
-    //     .populate('thoughts')
-    //     .then(function (dbSaved) {
-    //         res.json(dbSaved);
-    //         console.log('* *  I N D E X J S  R E Q   P A R A M S * *');
-    //         console.log(req.params);
-    //         console.log('*  * I N D E X J S  R E Q   P A R A M S * *');
-    //         console.log('****  WHAT WE HAVE ON DB - DBSAVED  ****')
-    //         console.log(dbSaved);
-    //         console.log('****  WHAT WE HAVE ON DB - DBSAVED THOUGHTS  ****')
-    //         console.log(dbSaved.thoughts);
-    //         console.log('****  WHAT WE HAVE ON DB  ****');
-    //         console.log('****  WHAT WE TO RENDER - DBSAVED THOUGHTS  ****')
-    //         console.log(dbSaved._id);
-    //         console.log('****  WHAT WE NEED TO RENDER ON DB  ****');
-    //     })
-    //     .catch(function (err) {
-    //         // If an error occurs, send it back to the client
-    //         res.json(err);
-    //         console.log('indexjs line 59 err - > ', err );
-    //     });
 });
 
 
 // Route for saving a new thought to the db and associating it with a User
 Router.post("/thoughts", function (req, res) {
-    // console.log('INDEXJS POST REQ.BODY BELOW');
     // console.log(req.body);
-    // console.log('INDEXJS POST REQ.BODY ABOVE');
-    // console.log('========================================');
-    // console.log('INDEXJS POST RES.BODY BELOW');
-    // console.log(res.body);
-    // console.log('INDEXJS POST RES.BODY ABOVE');
+
     // Create a new Thought in the db
     db.Thought.create(req.body)
         .then(function (dbThought) {
             // If a Thought was created successfully, find one User (there's only one) and push the new Thought's _id to the User's `thoughts` array
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            console.log("*+=*+=*INDEXJS DB THOUGHT*+=*+=*+");
-            console.log(dbThought);
-            console.log("*+=*+=*INDEXJS DB THOUGHT*+=*+=*+");
-            console.log('indexjs line 76 req body userId -> ', req.body.userId);
-            // .update({_id: contact.id}, upsertData
-            return db.User.updateOne({ userId: req.body.userId }, { $push: { thoughts: dbThought._id } }, { upsert: true });
-            // return db.User.findOneAndUpdate({}, { $push: { thoughts: dbThought._id }}, { new: true });
+            console.log("= INDEXJS DB THOUGHT =");
+            console.log(req.body.userId + ":" + dbThought);
 
-            // return 'YAHOO';
+            // pushes the new thought into the database with existing userId
+            // if the userId doesn't exist, create a new Userschema (upsert:true)
+            return db.User.updateOne({ userId: req.body.userId }, { $push: { thoughts: dbThought._id } }, { upsert: true });
+
         })
         .then(function (dbUser) {
             // If the User was updated successfully, send it back to the client
@@ -119,13 +90,11 @@ Router.post("/thoughts", function (req, res) {
         });
 });
 
-
+// get thoughts by their id
+// - testing purpose
 Router.get('/thoughts/:_id', function (req, res) {
     db.Thought.findById({ _id: req.params._id })
         .then(function (dbSaved) {
-            // console.log('++++++INDEXJS LINE 98 REQ.BODY BELOW+++++++++')
-            // console.log(req.body);
-            // console.log('++++++INDEXJS LINE 98 REQ.BODY ABEVR++++++++');
             res.json(dbSaved);
         })
         .catch(function (err) {
@@ -134,6 +103,8 @@ Router.get('/thoughts/:_id', function (req, res) {
         });
 });
 
+// delete a thought by its id
+// - testing purposes
 Router.delete('/thoughts/:_id', function (req, res) {
     console.log("DELETE")
     db.Thought.findByIdAndDelete({ _id: req.params._id })
@@ -145,6 +116,8 @@ Router.delete('/thoughts/:_id', function (req, res) {
         })
 });
 
+// gets all the thoughts
+// - testing purpose
 Router.get('/thoughts/all', function (req, res) {
     console.log("GET ALL THOUGHTS")
     db.Thought.find()
@@ -155,9 +128,6 @@ Router.get('/thoughts/all', function (req, res) {
         .catch(err => {
             console.log(err)
         })
-
 });
-
-
 
 module.exports = Router;
